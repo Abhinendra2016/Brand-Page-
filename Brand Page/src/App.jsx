3e1c063acf3f23882526be496d897592babbf035
ABHINENDRA PRATAP SINGH <abhinendrapratap2016@gmail.com>
2023-11-09 23:08:33 +0530

    setup

---
+++ Brand Page/src/App.jsx
@@ -0,0 +1,19 @@
+const App = () =>{
+  return(
+  <div> 
+    <nav>
+      <div className="logo">
+        <img src="./public/images/brand_logo.png" alt="logo"/>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+        <button>Login</button>
+      </div>
+
+    </nav>
+  </div>)
+}
+export default App
